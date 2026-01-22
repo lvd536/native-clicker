86e25dffaa30505430e68b8ce0aca8ec95356dd0
lvd536 <nbikiti@gmail.com>
2026-01-22 20:10:49 +0400

    - Change stone color

--- consts/colors.ts
+++ consts/colors.ts
@@ -3,6 +3,6 @@ export const colors = {
     indigo500: "#615fff",
     indigo600: "#4f39f6",
     indigo700: "#432dd7",
-    stone500: "#79716b",
+    stone500: "#79716bcc",
     background: "#191820",
 } as const;
